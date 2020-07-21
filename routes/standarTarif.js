bf24b6001f0a54243aba420b8ee3e1a61e2d56f3
Arief Yusron <ariefyusron98@gmail.com>
2020-07-22 06:34:55 +0700

    middleware check admin standar tarif

--- routes/standarTarif.js
+++ routes/standarTarif.js
@@ -5,9 +5,9 @@ const router = express.Router();
 const controller = require('../controllers/standarTarif');
 const middleware = require('../middlewares/auth');
 
-router.post('/', middleware.checkAuth, controller.createStandarTarif);
-router.patch('/:id', middleware.checkAuth, controller.updateStandarTarif);
-router.delete('/:id', middleware.checkAuth, controller.deleteStandarTarif);
+router.post('/', [middleware.checkAuth, middleware.checkAdmin], controller.createStandarTarif);
+router.patch('/:id', [middleware.checkAuth, middleware.checkAdmin], controller.updateStandarTarif);
+router.delete('/:id', [middleware.checkAuth, middleware.checkAdmin], controller.deleteStandarTarif);
 router.get('/', middleware.checkAuth, controller.getStandarTarif);
 
 module.exports = router;
