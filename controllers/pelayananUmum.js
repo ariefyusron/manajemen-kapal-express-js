4e41b3981985ded1f74de129484712bb1539c763
Arief Yusron <ariefyusron98@gmail.com>
2020-07-21 11:04:47 +0700

    bug fix

--- controllers/pelayananUmum.js
+++ controllers/pelayananUmum.js
@@ -1,14 +1,16 @@
 const models = require('../db/models');
 
 exports.createPelayananUmum = async (req, res) => {
-  const total = req.body.dps + req.body.sub_kont + req.body.jasa_peralatan + req.body.material + req.body.material_bantu;
+  // eslint-disable-next-line radix
+  const total = parseInt(req.body.dps) + parseInt(req.body.sub_kont) + parseInt(req.body.jasa_peralatan) + parseInt(req.body.material) + parseInt(req.body.material_bantu);
 
   const result = await models.PelayananUmum.create({ ...req.body, id_kapal: req.params.idKapal, total });
   res.json(result);
 };
 
 exports.updatePelayananUmum = async (req, res) => {
-  const total = req.body.dps + req.body.sub_kont + req.body.jasa_peralatan + req.body.material + req.body.material_bantu;
+  // eslint-disable-next-line radix
+  const total = parseInt(req.body.dps) + parseInt(req.body.sub_kont) + parseInt(req.body.jasa_peralatan) + parseInt(req.body.material) + parseInt(req.body.material_bantu);
   const result = await models.PelayananUmum.update({ ...req.body, total }, { where: { id: req.params.id } });
   if (result[0]) {
     res.json({ id: req.params.id, ...req.body, total });
--- controllers/pengedokan.js
+++ controllers/pengedokan.js
@@ -1,14 +1,16 @@
 const models = require('../db/models');
 
 exports.createPengedokan = async (req, res) => {
-  const total = req.body.dps + req.body.sub_kont + req.body.jasa_peralatan + req.body.material + req.body.material_bantu;
+  // eslint-disable-next-line radix
+  const total = parseInt(req.body.dps) + parseInt(req.body.sub_kont) + parseInt(req.body.jasa_peralatan) + parseInt(req.body.material) + parseInt(req.body.material_bantu);
 
   const result = await models.Pengedokan.create({ ...req.body, id_kapal: req.params.idKapal, total });
   res.json(result);
 };
 
 exports.updatePengedokan = async (req, res) => {
-  const total = req.body.dps + req.body.sub_kont + req.body.jasa_peralatan + req.body.material + req.body.material_bantu;
+  // eslint-disable-next-line radix
+  const total = parseInt(req.body.dps) + parseInt(req.body.sub_kont) + parseInt(req.body.jasa_peralatan) + parseInt(req.body.material) + parseInt(req.body.material_bantu);
   const result = await models.Pengedokan.update({ ...req.body, total }, { where: { id: req.params.id } });
   if (result[0]) {
     res.json({ id: req.params.id, ...req.body, total });
