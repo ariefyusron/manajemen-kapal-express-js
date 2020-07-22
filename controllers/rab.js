21f2893c11455e13dd00a7c39b4feae8030e51e4
Arief Yusron <ariefyusron98@gmail.com>
2020-07-22 15:28:09 +0700

    rab

--- controllers/rab.js
+++ controllers/rab.js
@@ -1,19 +1,36 @@
 const models = require('../db/models');
 
 exports.createRab = async (req, res) => {
-  // eslint-disable-next-line radix
-  const total = parseInt(req.body.dps) + parseInt(req.body.sub_kont) + parseInt(req.body.jasa_peralatan) + parseInt(req.body.material) + parseInt(req.body.material_bantu) + parseInt(req.body.overhead);
+  const dps = req.body.dps === '' ? 0 : parseInt(req.body.dps, 10);
+  const subKont = req.body.sub_kont === '' ? 0 : parseInt(req.body.sub_kont, 10);
+  const jasaPeralatan = req.body.jasa_peralatan === '' ? 0 : parseInt(req.body.jasa_peralatan, 10);
+  const material = req.body.material === '' ? 0 : parseInt(req.body.material, 10);
+  const materialBantu = req.body.material_bantu === '' ? 0 : parseInt(req.body.material_bantu, 10);
+  const overhead = req.body.overhead === '' ? 0 : parseInt(req.body.overhead, 10);
+  const total = dps + subKont + jasaPeralatan + material + materialBantu + overhead;
 
-  const result = await models.Rab.create({ ...req.body, id_kapal: req.params.idKapal, total });
+  const result = await models.Rab.create({
+    id_pekerjaan: req.body.id_pekerjaan, id_kapal: req.params.idKapal, dps, sub_kont: subKont, jasa_peralatan: jasaPeralatan, material, material_bantu: materialBantu, overhead, total
+  });
   res.json(result);
 };
 
 exports.updateRab = async (req, res) => {
-  // eslint-disable-next-line radix
-  const total = parseInt(req.body.dps) + parseInt(req.body.sub_kont) + parseInt(req.body.jasa_peralatan) + parseInt(req.body.material) + parseInt(req.body.material_bantu) + parseInt(req.body.overhead);
-  const result = await models.Rab.update({ ...req.body, total }, { where: { id: req.params.id } });
+  const dps = req.body.dps === '' ? 0 : parseInt(req.body.dps, 10);
+  const subKont = req.body.sub_kont === '' ? 0 : parseInt(req.body.sub_kont, 10);
+  const jasaPeralatan = req.body.jasa_peralatan === '' ? 0 : parseInt(req.body.jasa_peralatan, 10);
+  const material = req.body.material === '' ? 0 : parseInt(req.body.material, 10);
+  const materialBantu = req.body.material_bantu === '' ? 0 : parseInt(req.body.material_bantu, 10);
+  const overhead = req.body.overhead === '' ? 0 : parseInt(req.body.overhead, 10);
+  const total = dps + subKont + jasaPeralatan + material + materialBantu + overhead;
+
+  const result = await models.Rab.update({
+    id_pekerjaan: req.body.id_pekerjaan, dps, sub_kont: subKont, jasa_peralatan: jasaPeralatan, material, material_bantu: materialBantu, overhead, total
+  }, { where: { id: req.params.id } });
   if (result[0]) {
-    res.json({ id: req.params.id, ...req.body, total });
+    res.json({
+      id: req.params.id, id_pekerjaan: req.body.id_pekerjaan, dps, sub_kont: subKont, jasa_peralatan: jasaPeralatan, material, material_bantu: materialBantu, overhead, total
+    });
   }
   else {
     res.status(404).json({ message: 'Not found' });
@@ -47,4 +64,47 @@ exports.getByIdRab = async (req, res) => {
   else {
     res.status(404).json({ message: 'Not found' });
   }
+};
+
+// type pekerjaan
+exports.createPekerjaan = async (req, res) => {
+  const result = await models.PekerjaanRab.create({ ...req.body, id_kapal: req.params.idKapal });
+  res.json(result);
+};
+
+exports.deletePekerjaan = async (req, res) => {
+  await models.Rab.destroy({
+    where: {
+      id_pekerjaan: req.params.id
+    }
+  });
+
+  const result = await models.PekerjaanRab.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+  if (result) {
+    res.json({ message: 'Deleted' });
+  }
+  else {
+    res.status(404).json({ message: 'Not found' });
+  }
+};
+
+exports.updatePekerjaan = async (req, res) => {
+  const result = await models.PekerjaanRab.update(req.body, { where: { id: req.params.id } });
+  if (result[0]) {
+    res.json({
+      id: req.params.id, ...req.body
+    });
+  }
+  else {
+    res.status(404).json({ message: 'Not found' });
+  }
+};
+
+exports.getPekerjaan = async (req, res) => {
+  const result = await models.PekerjaanRab.findAll({ where: { id_kapal: req.params.idKapal } });
+  res.json(result);
 };
--- routes/rab.js
+++ routes/rab.js
@@ -11,4 +11,9 @@ router.delete('/:idKapal/:id', middleware.checkAuth, controller.deleteRab);
 router.get('/:idKapal', middleware.checkAuth, controller.getRab);
 router.get('/:idKapal/:id', middleware.checkAuth, controller.getByIdRab);
 
+router.post('/:idKapal/pekerjaan', middleware.checkAuth, controller.createPekerjaan);
+router.patch('/:idKapal/pekerjaan/:id', middleware.checkAuth, controller.updatePekerjaan);
+router.delete('/:idKapal/pekerjaan/:id', middleware.checkAuth, controller.deletePekerjaan);
+router.get('/:idKapal/pekerjaan', middleware.checkAuth, controller.getPekerjaan);
+
 module.exports = router;
