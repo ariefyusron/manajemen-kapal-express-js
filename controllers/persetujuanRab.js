4d6b011b6c75515c92065447c581ee939a7a5702
Arief Yusron <ariefyusron98@gmail.com>
2020-08-07 06:21:03 +0700

    bug fix

--- controllers/persetujuanRab.js
+++ controllers/persetujuanRab.js
@@ -1,9 +1,11 @@
 const models = require('../db/models');
 
 exports.update = async (req, res) => {
-  const result = await models.PersetujuanRab.update(req.body, { where: { id_kapal: req.params.idKapal } });
+  const result = await models.PersetujuanRab.update({ ...req.body, dok: req.body.dok === '' ? 0 : req.body.dok, floating: req.body.floating === '' ? 0 : req.body.floating }, { where: { id_kapal: req.params.idKapal } });
   if (result[0]) {
-    res.json({ id_kapal: req.params.idKapal, ...req.body });
+    res.json({
+      id_kapal: req.params.idKapal, ...req.body, dok: req.body.dok === '' ? 0 : req.body.dok, floating: req.body.floating === '' ? 0 : req.body.floating
+    });
   }
   else {
     res.status(404).json({ message: 'Not found' });
